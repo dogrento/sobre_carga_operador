9a6c0ed469a2776cb77e8f9cfdfe81bc56bfde51
dogrento <dogrento93@gmail.com>
2022-10-11 21:02:38 -0300

    comentando o role

--- minha_string.cpp
+++ minha_string.cpp
@@ -1,6 +1,6 @@
 #include "minha_string.h"
 
-
+// metodo construtora 
 MinhaString::MinhaString(const char* s):
 tamanho(strlen(s))
 {
@@ -10,13 +10,17 @@ tamanho(strlen(s))
 
 MinhaString::~MinhaString(){
     cout << "destruindo..."<<endl;
+    // operador para destruir um array e ponteiros 
     delete[]pString;
+    // provavelmente, não a necessidade de atribuir NULL ao ponteiro
     pString = NULL;
 }
 
 void MinhaString::Set_string(const char* s){
     cout << "setando string..." << endl;
+    cout << "criando vetor dinamicamente." << endl;
     pString = new char[tamanho + 1];
+    // copiando string para o novo ponteiro 
     strcpy(pString, s);
 }
 
@@ -24,13 +28,19 @@ const char* MinhaString::Get_string(){
     return pString;
 }
 
+// sobre carga de operador e criando regra para operador receber
 void MinhaString::operator=(const char* s){
+    //se conteudo do parametro for diferente do atributo
     if(s!=pString){
+        // destroi ponteiro
         delete[]pString;
+        // atributo tamanho receber o tamanho do que parametro
         tamanho = strlen(s);
+        // set nova string e coloca seu conteudo no atributo pString
         Set_string(s);
     }
 }
+
 void MinhaString::operator=(MinhaString& s){
     operator=(s.Get_string());
 }
